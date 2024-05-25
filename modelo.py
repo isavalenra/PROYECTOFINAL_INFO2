49100bb9c32799a35f2bb85974330fa848ebac27
luhernandez12 <luisam.hernandez1@udea.edu.co>
2024-05-25 18:03:55 -0500

    importaciones iniciales

---
+++ modelo.py
@@ -0,0 +1,4 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsDropShadowEffect
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import QPoint, Qt, QByteArray, QIODevice, QBuffer

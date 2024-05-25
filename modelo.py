294d1c3541469dc380ea549b6350ec810e5ac299
luhernandez12 <luisam.hernandez1@udea.edu.co>
2024-05-25 18:32:53 -0500

    crear clase con conexion de base de datos

--- modelo.py
+++ modelo.py
@@ -2,3 +2,21 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsDropShadowEffect
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QPoint, Qt, QByteArray, QIODevice, QBuffer
+
+#Establecer base de datos, hacer conexion y desconexion 
+
+class BaseDatos:
+    def __init__(self,nombre_db):
+        self.nombre_db=nombre_db
+        self.conexion=None
+        self.cursor=None
+    
+    def conexion(self):  #Se realiza la conexion 
+        self.conexion=sqlite3.connect(self.nombre_db)
+        self.cursor=self.conexion.cursor()
+        print("La conexion se realizo exitosamente")
+    
+    def desconexion(self):  #se realiza la desconexion 
+        if self.conexion:
+            self.conexion.close()
+            print(f"Se desconecto de la base de datos:{self.nombre_db}")
